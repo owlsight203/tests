4cc0adda7b156a1fa3bb26cd17d67c46c0fb1fcd
owlsight203 <drkcrw203@gmail.com>
2026-08-11 11:48:23 +0700

    test: Add 4 tests including click interaction

--- test_blog.py
+++ test_blog.py
@@ -1,3 +1,4 @@
+import re
 from playwright.sync_api import Page, expect
 
 def test_homepage_load(page: Page):
@@ -32,3 +33,18 @@ def test_post_detail_id(page: Page):
 
 	submit_btn = page.locator("button, input[type='submit']").first
 	expect(submit_btn).to_be_visible()
+
+def test_click_post(page: Page):
+	
+	page.goto("127.0.0.1:8080")
+
+	first_link = page.get_by_role("link", name = "Xem chi tiết").first
+	expect(first_link).to_be_visible()
+
+	first_link.click()
+
+	expect(page).to_have_url(re.compile(r".*/post/\d+"))
+
+	title = page.locator("h1, h5").first
+
+	expect(title).to_be_visible()
