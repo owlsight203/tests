e300aa8cd0a6309bb532c89d0a76255b6dfc0560
owlsight203 <drkcrw203@gmail.com>
2026-08-12 14:21:57 +0700

    test content form

--- test_blog.py
+++ test_blog.py
@@ -1,50 +1,63 @@
 import re
 from playwright.sync_api import Page, expect
 
-def test_homepage_load(page: Page):
-	page.goto("127.0.0.1:8080")
-	body = page.locator("body")
-	expect(body).to_be_visible()
+# def test_homepage_load(page: Page):
+# 	page.goto("127.0.0.1:8080")
+# 	body = page.locator("body")
+# 	expect(body).to_be_visible()
 
-	# title = page.title() 
+# 	# title = page.title() 
 
-	# assert len(title) > 0
+# 	# assert len(title) > 0
 
-	title = page.locator("h1").first
-	expect(title).to_be_visible()
+# 	title = page.locator("h1").first
+# 	expect(title).to_be_visible()
 
 
-def test_post_detail(page: Page):
-	page.goto("127.0.0.1:8080")
-	body = page.locator("body")
-	expect(body).to_be_visible()
+# def test_post_detail(page: Page):
+# 	page.goto("127.0.0.1:8080")
+# 	body = page.locator("body")
+# 	expect(body).to_be_visible()
 
-	title_content = page.locator("h5").first
-	expect(title_content).to_be_visible()
+# 	title_content = page.locator("h5").first
+# 	expect(title_content).to_be_visible()
 
-def test_post_detail_id(page: Page):
+# def test_post_detail_id(page: Page):
+# 	page.goto("127.0.0.1:8080/post/14")
+	
+# 	body = page.locator("body")
+# 	expect(body).to_be_visible()
+
+# 	content_box = page.locator("textarea, input[name='content']").first
+# 	expect(content_box).to_be_visible()
+
+# 	submit_btn = page.locator("button, input[type='submit']").first
+# 	expect(submit_btn).to_be_visible()
+
+# def test_click_post(page: Page):
+	
+# 	page.goto("127.0.0.1:8080")
+
+# 	first_link = page.get_by_role("link", name = "Xem chi tiết").first
+# 	expect(first_link).to_be_visible()
+
+# 	first_link.click()
+
+# 	expect(page).to_have_url(re.compile(r".*/post/\d+"))
+
+# 	title = page.locator("h1, h5").first
+
+# 	expect(title).to_be_visible()
+
+def test_content_form(page: Page):
 	page.goto("127.0.0.1:8080/post/14")
-	
-	body = page.locator("body")
-	expect(body).to_be_visible()
 
-	content_box = page.locator("textarea, input[name='content']").first
-	expect(content_box).to_be_visible()
+	content_box = page.locator("textarea[name='content']").first
+	content_box.fill("Bình luận từ Playwright")
 
-	submit_btn = page.locator("button, input[type='submit']").first
-	expect(submit_btn).to_be_visible()
+	gemini_radio = page.locator("input#ai_gemini").first
+	gemini_radio.click()
 
-def test_click_post(page: Page):
-	
-	page.goto("127.0.0.1:8080")
+	expect(content_box).to_have_value("Bình luận từ Playwright")
+	expect(gemini_radio).to_be_checked()
 
-	first_link = page.get_by_role("link", name = "Xem chi tiết").first
-	expect(first_link).to_be_visible()
-
-	first_link.click()
-
-	expect(page).to_have_url(re.compile(r".*/post/\d+"))
-
-	title = page.locator("h1, h5").first
-
-	expect(title).to_be_visible()
